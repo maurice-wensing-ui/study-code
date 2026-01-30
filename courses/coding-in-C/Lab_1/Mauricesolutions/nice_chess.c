b07c80cf307fd145982d31402e14039a6611f2cb
maurice-wensing-ui <maurice-wensing@t-online.de>
2026-01-30 17:03:19 +0000

    ChessCode2

---
+++ courses/coding-in-C/Lab_1/Mauricesolutions/nice_chess.c
@@ -0,0 +1,134 @@
+#include <stdio.h>
+
+char charBoardGrenze; // Board Grenze und Linien
+char charSpalten; // Spalten beschriftung
+int intZeilen; // For Schleife für Zeilen
+int intZeilenNummer; // Zeilen nummerierung
+char charWeiß_Schwarz; //Schleife für Muster
+int intVariable = 9; // Zeilen nummerierung
+int inti = 0; // zählt die Zeilen
+int intg = 0; // zählt die Spalten
+
+int main()
+{
+
+/*----------------------------------------------------------------*/
+// Obere Beschriftung
+/*----------------------------------------------------------------*/
+
+    {
+    
+        for (charBoardGrenze = 0; charBoardGrenze < 10; charBoardGrenze++)
+        {
+                printf("+-----");
+        }
+        printf("+\n");
+
+
+        printf("|     ");
+        for (charSpalten = 'A'; charSpalten <= 'H'; charSpalten++)
+            {
+                printf("|  %c  ", charSpalten);
+            }
+            printf("|     |\n");
+
+                for (charBoardGrenze = 0; charBoardGrenze < 10; charBoardGrenze++)
+            {
+                printf("+-----");
+            }
+            printf("+\n");
+    }
+
+/*----------------------------------------------------------------*/
+// SChleife für Zeilen
+/*----------------------------------------------------------------*/
+
+for (intZeilen  = 2; intZeilen  >= 1; intZeilen--)
+{
+
+/*----------------------------------------------------------------*/
+// Schleife für Spalten
+/*----------------------------------------------------------------*/
+
+for (intZeilenNummer = 1; intZeilenNummer <= 9; intZeilenNummer++)
+{
+    if (inti%2 == 1) // Zeile mit Nummerirung
+    {
+        intVariable--;
+        printf("|  %d  |", intVariable); // Zeilen nummerierung vorne
+
+    /*----------------------------------------------------------------*/
+    // Muster von von Zeile 8 bis 0
+    /*----------------------------------------------------------------*/
+
+        for (charWeiß_Schwarz = 0; charWeiß_Schwarz < 8; charWeiß_Schwarz++)
+        {
+            intg++; // zählt die Spalten
+
+            if (intg%2 == 0)
+            {
+        
+                printf(" ### |");
+            }
+
+            else if (intg%2 == 1)
+            {
+            
+            printf("     |");
+            
+            }
+           
+        } 
+
+    /*----------------------------------------------------------------*/
+    // Zeilen nummerireung hinten
+    /*----------------------------------------------------------------*/
+
+        printf("  %d  |\n", intVariable);
+        intg++; // +1 damit das Muster entsteht
+    /*----------------------------------------------------------------*/
+    // Zeile Über der unteren Beschriftung
+    /*----------------------------------------------------------------*/
+
+        if (inti%2 == 1){
+
+                for (charBoardGrenze = 0; charBoardGrenze < 10; charBoardGrenze++)
+                    {
+                        printf("+-----");
+                    }
+                printf("+\n");
+            }
+
+        }
+        inti++; // zählt die Spalten
+
+
+    }
+
+}
+
+/*----------------------------------------------------------------*/
+// Untere Beschriftung
+/*----------------------------------------------------------------*/
+
+    {        
+        
+        printf("|     ");
+        for (charSpalten = 'A'; charSpalten <= 'H'; charSpalten++)
+            {
+                printf("|  %c  ", charSpalten);
+            }
+            printf("|     |\n");
+
+        for (charBoardGrenze = 0; charBoardGrenze < 10; charBoardGrenze++)
+            {
+                printf("+-----");
+            }
+            printf("+\n");
+
+    }
+
+/*----------------------------------------------------------------*/
+
+return 0;
+}
