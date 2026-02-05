6861609d00e95a36616aae3181c05e29f64c53e6
maurice-wensing-ui <maurice-wensing@t-online.de>
2026-02-05 17:49:23 +0000

    Taschenrechner gebaut

---
+++ courses/coding-in-C/Lab_1/Mauricesolutions/Switch_Taschenrechner.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+
+
+
+float a;
+float b;
+float c;
+char operator;
+
+/*----------------------------------------------------------------*/
+// Einlesen
+/*----------------------------------------------------------------*/
+
+void einlesen()
+{
+    printf("Welche Operation möchten Sie durchführen?(+, -, *, /)\n");
+    scanf(" %c", &operator);
+
+    printf("Geben Sie die 1. Zahl an.\n");
+    scanf("%f", &a);
+
+    printf("Geben Sie die 2. Zahl an.\n");
+    scanf("%f", &b);
+};
+
+
+/*----------------------------------------------------------------*/
+// Operator auswahl
+/*----------------------------------------------------------------*/
+void operator_auswahl()
+{
+switch (operator)
+{
+case '+':
+    c = a + b;
+    break;
+
+case '-':
+    c = a - b;
+    break;
+
+case '*':
+    c = a * b;
+    break;
+
+case '/':
+    c = a / b;
+    break;
+
+default:
+    printf("Ungültiger Operator.");
+    break;
+
+}    
+}
+
+/*----------------------------------------------------------------*/
+// Ausgabe
+/*----------------------------------------------------------------*/
+
+void ausgabe()
+{
+
+    if (b == 0 &&
+             operator == '/')
+    {
+        printf("Mit 0 kann nicht geteilt werden.");
+    }
+    else
+    printf("%f %c %f = %f",a, operator, b, c);
+};
+
+
+/*----------------------------------------------------------------*/
+// Main
+/*----------------------------------------------------------------*/
+
+int main()
+{
+
+einlesen();
+
+operator_auswahl();
+
+
+ausgabe();
+
+return 0;
+
+}
