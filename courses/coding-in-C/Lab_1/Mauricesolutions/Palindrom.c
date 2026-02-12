b1ff3a9fb3a05260663bead3638ffb93411ec79e
maurice-wensing-ui <maurice-wensing@t-online.de>
2026-02-12 13:22:33 +0000

    Palindrom vergleich

--- courses/coding-in-C/Lab_1/Mauricesolutions/Palindrom.c
+++ courses/coding-in-C/Lab_1/Mauricesolutions/Palindrom.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+
+char Palindrom[100];
+char array[100];
+
 /*----------------------------------------------------------------*/
 // Satz bilden
 /*----------------------------------------------------------------*/
@@ -31,18 +35,71 @@ printf("%s\n", Satz);
 
 void input_string()
 {
+    char *ptr2 = &Palindrom[1];
+    
+    printf("Geben Sie das Palindrom ein?\n");
 
+    fgets(Palindrom, 100, stdin);
+    Palindrom[strcspn(Palindrom, "\n")] = '\0';
+
+    //*ptr2 = 'c';
+
+    //printf("Eingegeben: %s\n", Palindrom);
 }
 
 /*----------------------------------------------------------------*/
-// Input_String
+// Palindrom prüfung
 /*----------------------------------------------------------------*/
+
+void palindrom_prüfung()
+{
+char *ptr3;
+int länge = strlen(Palindrom);
+ptr3 = &Palindrom[länge-1];
+
+
+for (int i = 0; i < länge; i++)
+{
+    array[i] = *ptr3;
+    ptr3--;
+}
+
+array[länge] = '\0';
+
+};
+
+/*----------------------------------------------------------------*/
+// Output
+/*----------------------------------------------------------------*/
+
+void output()
+{
+
+printf("Eingegeben: %s\nPalindromarray: %s\n", Palindrom, array);
+
+if (strcmp(Palindrom, array) == 0)
+{
+    printf("%s ist ein Palindrom", Palindrom);
+}
+
+else
+{
+    printf("%s ist kein Palindrom", Palindrom);
+
+};
+};
 
 
 int main()
 {
 
 Satz_bilden();
+
+input_string();
+
+palindrom_prüfung();
+
+output();
 
  return 0;
 }
